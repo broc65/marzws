26c57743f97cbf3e1dae3b6bf4ded00e8bb62ca1
broc <126756992+broc65@users.noreply.github.com>
2023-11-20 16:12:49 +0700

    Add files via upload

---
+++ clear.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+rm -rf /var/log/*.log
+rm -rf /var/log/*.1
+rm -rf /var/log/*.gz
+rm -rf /var/log/apt/*.log
+rm -rf /var/log/apt/*.xz
+rm -rf /var/log/nginx/*.log
+rm -rf /var/log/nginx/*.1
+rm -rf /var/log/nginx/*.gz
+rm -rf /var/lib/marzban/*.log
