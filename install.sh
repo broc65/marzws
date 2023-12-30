4a6b68ff35761376d1cf4aaca4b3f907bac201cd
broc <126756992+broc65@users.noreply.github.com>
2023-12-31 00:10:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -140,12 +140,13 @@ cd /opt/marzban
 docker compose down && docker compose up -d
 cd
 rm /root/install.sh
+clear
 
-echo -e '\e[32;1m============================================================\e[0m'
-echo -e '\e[0;32m                  Installation Success!                     \e[0m'
-echo -e '\e[32;1m============================================================\e[0m'
+echo -e "\e[32;1m============================================================\e[0m"
+echo -e "\e[0;32m                   Installation Success!                    \e[0m"
+echo -e "\e[32;1m============================================================\e[0m"
 sleep 3
-echo -e "\e[33;1m[ WARNING ] reboot now ? (Y/N)\e[0m"
+echo -e "\e[33;1m===============[ WARNING ] reboot now ? (Y/N)===============\e[0m"
 read answer
 if [ "$answer" == "${answer#[Yy]}" ] ;then
 exit 0
