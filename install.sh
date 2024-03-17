06c29e646045c9eefb72c76c7c2dea40c2dd60ce
broc <126756992+broc65@users.noreply.github.com>
2024-03-17 12:45:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -122,11 +122,9 @@ apt install socat cron bash-completion -y
 #install cert
 domain=$(cat /root/domain)
 systemctl stop nginx
-curl https://get.acme.sh | sh
-source ~/.bashrc
-cd .acme.sh
-bash acme.sh --issue -d $domain --server letsencrypt --keylength ec-256 --fullchain-file /var/lib/marzban/xray.crt --key-file /var/lib/marzban/xray.key --standalone --force
-cd
+curl https://get.acme.sh | sh -s email=$email
+/root/.acme.sh/acme.sh --server letsencrypt --register-account -m $email --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /var/lib/marzban/xray.crt --keypath /var/lib/marzban/xray.key --ecc
 systemctl start nginx
 wget -O /var/lib/marzban/xray_config.json "https://raw.githubusercontent.com/broc65/marzws/main/xray_config.json"
 
